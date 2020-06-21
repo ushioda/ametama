9bb75c3c331e3f12a38df08dd6d2c5f77145e1ae
Yu Ushioda <yuu1023@gmail.com>
2020-06-21 18:48:49 -0500

    changed fonts

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 const Color kNavy = Color(0xff1c306d);
-const Color kDarkYellow = Color(0xfff9b32f);
-const Color kLightYellow = Color(0xffffcf4b);
+const Color kDarkYellow = Color(0xffFF8F00);
+const Color kLightYellow = Color(0xffFFB300);
+const Color kBackGround = Color(0xfffff8e1);
 const Color kMyShadow = Color(0xffababab);
 
 const String kNotoSansJP = 'NotoSansJP';
 const String kKosugiMaru = 'KosugiMaru';
+
+const double kResultDisplayNumberSize1 = 30.0;
+const double kResultDisplayFontSize = 20.0;
--- lib/screens/top_screen.dart
+++ lib/screens/top_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ametama/constants.dart';
 import 'package:ametama/models/date_data.dart';
 import 'package:ametama/models/date_converter.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,7 @@ class TopScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: kBackGround,
       body: SafeArea(
         child: Column(
           children: <Widget>[
@@ -21,54 +22,190 @@ class TopScreen extends StatelessWidget {
               flex: 7,
               child: Consumer<DateData>(
                 builder: (context, dateData, child) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  return Column(
                     children: <Widget>[
-                      Column(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
                           Text(
                             'パック日',
-                            style: TextStyle(fontSize: 30),
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: kKosugiMaru,
+                              color: kDarkYellow,
+                            ),
                           ),
                           Text(
-                            dateData.packedDate != null
-                                ? '${dateData.packedDate.month.toString()}月${dateData.packedDate.day.toString()}日'
-                                : '',
-                            style: TextStyle(fontSize: 30),
-                          ),
-                          Text(
-                            dateData.packedDate != null
-                                ? '（${DateConverter().julianToString(dateData.julianDate)}）'
-                                : '',
-                            style: TextStyle(fontSize: 30),
+                            '消費期限',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: kKosugiMaru,
+                              color: kDarkYellow,
+                            ),
                           ),
                         ],
                       ),
-                      Column(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
-                          Text(
-                            '消費期限',
-                            style: TextStyle(fontSize: 30),
+                          Column(
+                            children: <Widget>[
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.baseline,
+                                textBaseline: TextBaseline.alphabetic,
+                                children: <Widget>[
+                                  Text(
+                                    dateData.expirationDate != null
+                                        ? '${dateData.packedDate.month.toString()}'
+                                        : '',
+                                    style: TextStyle(
+                                      fontSize: kResultDisplayNumberSize1,
+                                      color: kDarkYellow,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 1,
+                                  ),
+                                  Text(
+                                    '月',
+                                    style: TextStyle(
+                                        fontSize: kResultDisplayFontSize,
+                                        fontFamily: kKosugiMaru,
+                                        color: kDarkYellow),
+                                  ),
+                                  SizedBox(
+                                    width: 2,
+                                  ),
+                                  Text(
+                                    dateData.expirationDate != null
+                                        ? '${dateData.packedDate.day.toString()}'
+                                        : '',
+                                    style: TextStyle(
+                                      fontSize: kResultDisplayNumberSize1,
+                                      color: kDarkYellow,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 1,
+                                  ),
+                                  Text(
+                                    '日',
+                                    style: TextStyle(
+                                      fontSize: kResultDisplayFontSize,
+                                      fontFamily: kKosugiMaru,
+                                      color: kDarkYellow,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.baseline,
+                                textBaseline: TextBaseline.alphabetic,
+                                children: <Widget>[
+                                  Text(
+                                    dateData.packedDate != null
+                                        ? '（${DateConverter().julianToString(dateData.julianDate)}）'
+                                        : '',
+                                    style: TextStyle(
+                                      fontSize: 25,
+                                      color: kDarkYellow,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                          Text(
-                            dateData.expirationDate != null
-                                ? '${dateData.expirationDate.month.toString()}月${dateData.expirationDate.day.toString()}日'
-                                : '',
-                            style: TextStyle(fontSize: 30),
-                          ),
-                          Text(
-                            dateData.expirationDate != null
-                                ? (dateData.daysUntilExpiration > 0
-                                    ? '${dateData.daysUntilExpiration}日後'
-                                    : '${-dateData.daysUntilExpiration}日前')
-                                : '',
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: dateData.expirationDate != null
-                                    ? (dateData.daysUntilExpiration > 0
-                                        ? Colors.blueAccent
-                                        : Colors.redAccent)
-                                    : Colors.white),
+                          Column(
+                            children: <Widget>[
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.baseline,
+                                textBaseline: TextBaseline.alphabetic,
+                                children: <Widget>[
+                                  Text(
+                                    dateData.expirationDate != null
+                                        ? '${dateData.expirationDate.month.toString()}'
+                                        : '',
+                                    style: TextStyle(
+                                      fontSize: kResultDisplayNumberSize1,
+                                      color: kDarkYellow,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 1,
+                                  ),
+                                  Text(
+                                    '月',
+                                    style: TextStyle(
+                                        fontSize: kResultDisplayFontSize,
+                                        fontFamily: kKosugiMaru,
+                                        color: kDarkYellow),
+                                  ),
+                                  SizedBox(
+                                    width: 2,
+                                  ),
+                                  Text(
+                                    dateData.expirationDate != null
+                                        ? '${dateData.expirationDate.day.toString()}'
+                                        : '',
+                                    style: TextStyle(
+                                      fontSize: kResultDisplayNumberSize1,
+                                      color: kDarkYellow,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 1,
+                                  ),
+                                  Text(
+                                    '日',
+                                    style: TextStyle(
+                                      fontSize: kResultDisplayFontSize,
+                                      fontFamily: kKosugiMaru,
+                                      color: kDarkYellow,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.baseline,
+                                textBaseline: TextBaseline.alphabetic,
+                                children: <Widget>[
+                                  Text(
+                                    dateData.expirationDate != null
+                                        ? (dateData.daysUntilExpiration > 0
+                                            ? '${dateData.daysUntilExpiration}'
+                                            : '${-dateData.daysUntilExpiration}')
+                                        : '',
+                                    style: TextStyle(
+                                        fontSize: kResultDisplayNumberSize1,
+                                        fontWeight: FontWeight.bold,
+                                        color: dateData.expirationDate != null
+                                            ? (dateData.daysUntilExpiration > 0
+                                                ? Colors.blueAccent
+                                                : Colors.redAccent)
+                                            : Colors.white),
+                                  ),
+                                  SizedBox(
+                                    width: 1,
+                                  ),
+                                  Text(
+                                    dateData.expirationDate != null
+                                        ? (dateData.daysUntilExpiration > 0
+                                            ? '日後'
+                                            : '日前')
+                                        : '',
+                                    style: TextStyle(
+                                        fontFamily: kKosugiMaru,
+                                        fontSize: kResultDisplayFontSize,
+                                        fontWeight: FontWeight.bold,
+                                        color: dateData.expirationDate != null
+                                            ? (dateData.daysUntilExpiration > 0
+                                                ? Colors.blueAccent
+                                                : Colors.redAccent)
+                                            : Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ],
                       ),
