c1aac0fdc01492a37f13ab6ad14513d111d72107
Yu Ushioda <yuu1023@gmail.com>
2020-06-28 19:58:57 -0500

    changed app display name

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 import 'package:ametama/models/date_data.dart';
 import 'package:ametama/screens/top_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// make sure that the screen does not rotate
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) => runApp(MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
